8587b79f8f83edf48322e40c92b4b8c768ac200b
ShyLee873 <shylee873@gmail.com>
2021-11-05 13:44:59 -0400

    Completed REST Intro and Authentication

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -16,6 +16,10 @@ Devise.setup do |config|
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '82f94e081709bc58082c01d3b080b15c414df8898184b6b1462aa6cddd305006b35b96fbbe7a4672fc17213eafa82779c7008df8354c1934bebc1c46a503fded'
 
+  config.jwt do |jwt|
+    jwt.secret = Rails.application.credentials.devise[:jwt_secret_key]
+  end
+  
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
